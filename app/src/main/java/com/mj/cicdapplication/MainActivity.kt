99bb2449b6e9a308cee28d3de89a64ef2a89f833
mrutyunjay <mrutyunjay.senapati@ideagen.com>
2025-08-04 14:40:49 +0530

    update grettings

--- app/src/main/java/com/mj/cicdapplication/MainActivity.kt
+++ app/src/main/java/com/mj/cicdapplication/MainActivity.kt
@@ -3,6 +3,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -31,10 +32,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        Text(
+            text = "Hello CICD!",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
